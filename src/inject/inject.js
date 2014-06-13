c849e1d00f990b391059e3c469b8a6bc8639fa64
Kotaro Kawashima <utageyanagi+kkgithub@gmail.com>
2014-06-13 16:20:59 +0900

    全然きりよくないけどとりあえずSAVEがてらcommit

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -8,115 +8,146 @@ chrome.extension.sendMessage({}, function(response) {
       console.log("Hello. This message was sent from scripts/inject.js");
       // ----------------------------------------------------------
 
-      var app = app || {};
 
-      //create searchform
-      function createSearchForm(){
-        var dom$ = $("<div />").attr("id", "uw_searchform");
-        var ul$ = $("<ul />").addClass("userlist");
+      var add = function(func){
+        location.href="javascript: setTimeout(" + func.toString() + ", 10);";
+      };
 
-        dom$.html(ul$);
+      function run(){
+        /* set global bars */
+        var app = app || {};
+        app.keyCode = {
+          "@": 219,
+          "up": 38,
+          "down": 40,
+          "return": 13
+        };
+        app.userList = [];
+        app.selectUserIndex = 0;
 
-        return dom$;
-      }
+        /* set functions */
+        /* for dom */
+        app.createSearchForm = function(){
+          var dom$ = $("<div />").attr("id", "uw_searchform");
+          var ul$ = $("<ul />").addClass("userlist");
 
-      //set searchform
-      function setSearchForm(){
-        var searchform$ = createSearchForm();
-        searchform$.css({
-          position: "absolute",
-          top: 0,
-          right: 0,
-          display: "none"
-        });
+          dom$.html(ul$);
+          ul$.on("click", "li", app.selectedUser);
 
-        $("#_chatTextArea").append( searchform$ );
-        return searchform$;
-      }
+          return dom$;
+        };
 
-      var searchform$ = setSearchForm();
-
-      function loadGlobalVars(){
-        var s = $("body").find("script").first();
-        app.p = {};
-        var s2 = s.text().replace(/var /g, "app.p.");
-        console.log(s.text(), s2);
-        eval(s2);
-
-        console.log(app.p.myid);
-      }
-
-      function getFirstRoomId(){
-        return 0;
-      }
-
-      function init(){
-        loadGlobalVars();
-
-        app.p.rid = getFirstRoomId();
-
-        $.ajax({
-          url: "gateway.php",
-          data: {
-            cmd:"init_load",
-            myid: app.p.myid,
-            _v: app.p.client_ver,
-            _av:4,
-            _t: app.p.ACCESS_TOKEN,
-            ln:"ja",
-            rid: app.p.rid,
-            type: "",
-            new:1
-          },
-          dataType: "json",
-          type: "get"
-        }).then(function(data){
-          console.log("ajax data", data);
-          RL = {};
-          AC = {};
-          RL.rooms = data.result.room_dat;
-          $.each( RL.rooms, function(key, value){
-            //console.log(arguments, value, value.m);
-            var members = $.map(value.m, function(value, key){
-              return key;
-            });
-            members.sort();
-            value.sorted_member_list = members;
+        app.setSearchForm = function(){
+          var searchform$ = app.createSearchForm();
+          searchform$.css({
+            position: "absolute",
+            top: 0,
+            right: 0,
+            height: "100%",
+            overflow: "scroll",
+            display: "none"
           });
-          //console.log(RL.rooms);
-          AC.account_dat = data.result.contact_dat;
-        });      }
 
-      init();
+          $("#_chatTextArea").append( searchform$ );
+          return searchform$;
+        };
 
-      $("#_roomListItems").on("click", "li._roomLink", function(){
-        //console.log($(this), $(this).data("rid"));
-        RL.focused_room_id = $(this).data("rid");
-      });
+        app.createUserList = function(){
+          var currentRoom = RL.rooms[RL.focused_room_id];
+          app.userList = [];
+          var ul$ = app.searchform$.find("ul");
+          $.each(currentRoom.sorted_member_list, function(){
+            var user = AC.account_dat[this.toString()];
+            app.userList.push( user );
+            var li$ = $("<li />");
+            li$.text( user.nm );
+            ul$.append(li$);
+          }); 
+        };
 
+        app.closeUserList = function(){
+          app.selectMode = false;
+          app.searchform$.hide();
+        };
 
-      $("#_chatText").on("keyup", function(){
-        //console.log(this, this.value, this.value.split("\n"));
-        var splittext = this.value.split("\n");
-        $.each(splittext, function(index, text){
-          if(/^@$/.test(text)){
-            var currentRoom = RL.rooms[RL.focused_room_id];
-            //console.log("RL", window.RL, RL.focused_room_id, currentRoom);
-            var userlist = [];
-            var ul$ = searchform$.find("ul");
-            $.each(currentRoom.sorted_member_list, function(){
-              var user = AC.account_dat[this.toString()];
-              userlist.push( user.nm );
-              var li$ = $("<li />");
-              li$.text( user.nm );
-              ul$.append(li$);
-            }); 
-            searchform$.css({
-              display: "block"
-            });
+        app.openUserList = function(){
+          app.selectMode = true;
+          app.searchform$.show();
+        };
+
+        app.selectUser = function(index){
+          app.selectUserIndex = index;
+          app.searchform$.find("li").removeClass("select").eq(index).addClass("select");
+        };
+
+        /* for user action */
+        app.selectedUser = function(){
+          var t$ = $(this);
+          console.log("clicked", t$.text());
+        };
+
+        app.changeRoom = function(){
+          app.closeUserList();
+          app.createUserList();
+        };
+
+        app.onKey = function(e){
+          /*console.log("keyup", this, arguments, event);*/
+          console.log("keyup", event.keyCode);
+          var keycode = event.keyCode;
+
+          if( app.selectMode === true ){
+            var userlen = app.userList.length;
+            if( keycode === app.keyCode["down"]){
+              app.selectUser( (app.selectUserIndex + 1 ) % userlen );
+            }else if (keycode === app.keyCode["up"]){
+              app.selectUser( (app.selectUserIndex + userlen - 1 ) % userlen );
+            }else if (keycode === app.keyCode["return"]){
+              app.closeUserList();
+            }
+          }else{
+            if( keycode === app.keyCode["@"] ){
+              app.openUserList();
+              app.selectUser(0);
+            }
           }
-        });
-      });
+
+        };
+
+        /* for init */
+        app.init = function(){
+          app.searchform$ = app.setSearchForm();
+          app.loadGlobalVars();
+          app.p.rid = app.getFirstRoomId();
+          app.setEvents();
+          app.createUserList();
+        };
+
+        app.loadGlobalVars = function(){
+          var s = $("body").find("script").first();
+          app.p = {};
+          var s2 = s.text().replace(/var /g, "app.p.");
+          console.log(s.text(), s2);
+          eval(s2);
+
+          console.log(app.p.myid);
+        };
+
+        app.getFirstRoomId = function(){
+          return 0;
+        };
+
+        /* set Event */
+        app.setEvents = function(){
+          $("#_roomListItems").on("click", "li._roomLink", app.changeRoom);
+          $("#_chatText").on("keyup", app.onKey);
+        };
+
+        /* run */
+        app.init();
+      };
+
+      add( run );
 
     }
   }, 10);
